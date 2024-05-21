253d2144077bfe5db9b139842652988115cafaf2
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-05-21 14:14:52 +0200

    finished work

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 import sys
+import threading
 from scapy.all import get_if_list
-from my_sniffer import sniff_packets
+from my_sniffer import sniff_packets, save_packets_to_file
 
 # Inicjalizacja Pygame
 pygame.init()
@@ -9,22 +10,26 @@ pygame.init()
 # Ustawienia kolorów
 BACKGROUND_COLOR = (72, 80, 185)
 BUTTON_COLOR = (85, 224, 163)
+DISABLED_BUTTON_COLOR = (150, 150, 150)  # Kolor nieaktywnego przycisku
 SELECTED_BUTTON_COLOR = (200, 100, 255)  # Kolor wybranego przycisku
 TEXT_COLOR = (255, 255, 255)
 
 # Ustawienia okna
 WIDTH, HEIGHT = 1200, 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Przyciski w Pygame")
+pygame.display.set_caption("Packet Sniffer with GUI")
+
+font = pygame.font.SysFont(None, 20)
 
 
 # Funkcja rysująca przycisk
-def draw_button(x, y, width, height, color, text, selected=False):
+def draw_button(x, y, width, height, color, text, selected=False, enabled=True):
     if selected:
         color = SELECTED_BUTTON_COLOR
+    if not enabled:
+        color = DISABLED_BUTTON_COLOR
     pygame.draw.rect(screen, color, (x, y, width, height), border_radius=10)
 
-    font = pygame.font.SysFont(None, 25)
     text_surface = font.render(text, True, TEXT_COLOR)
     text_rect = text_surface.get_rect(center=(x + width / 2, y + height / 2))
     screen.blit(text_surface, text_rect)
@@ -37,70 +42,61 @@ def get_interfaces():
 
 # Główna pętla gry
 def main():
-    # Pobranie interfejsów
-    global selected_button
     interfaces = get_interfaces()
 
-    # Szerokość przycisku
-    button_width = (WIDTH - 20) // len(interfaces)  # Uwzględniamy odstępy
-
-    # Stworzenie listy przycisków na podstawie liczby interfejsów
+    button_width = (WIDTH - 20) // len(interfaces)
     buttons = []
     x, y = 10, HEIGHT // 4
     for interface in interfaces:
         button_rect = pygame.Rect(x, y, button_width, 50)
-        buttons.append((button_rect, True))  # Tutaj dodajemy informację czy przycisk jest aktywny czy nie
-        x += button_width + 10  # Dodajemy odstęp między przyciskami
-        if x + button_width > WIDTH:  # Jeśli przycisk wyjdzie poza ekran, przechodzimy do następnej linii
+        buttons.append((button_rect, True))
+        x += button_width + 10
+        if x + button_width > WIDTH:
             x = 10
-            y += 60  # Dodajemy odstęp między liniami przycisków
+            y += 60
 
-    # Dodanie przycisku "Start"
     start_button_rect = pygame.Rect(WIDTH // 2 - 50, HEIGHT - 80, 100, 50)
-    start_button = (start_button_rect, False)  # Start button initially inactive
+    start_button = (start_button_rect, False)
+
+    selected_button = None
+    stop_event = threading.Event()
 
     while True:
         screen.fill(BACKGROUND_COLOR)
-
-        # Napis "Wybierz interfejs"
-        font = pygame.font.SysFont(None, 30)
-        text_surface = font.render("Wybierz interfejs", True, TEXT_COLOR)
-        text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 4 - 25))  # Mniejszy odstęp
+        font_big = pygame.font.SysFont(None, 30)
+        text_surface = font_big.render("Wybierz interfejs", True, TEXT_COLOR)
+        text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 4 - 25))
         screen.blit(text_surface, text_rect)
 
-        # Rysowanie przycisków
         for button_data in buttons:
             button, active = button_data
             draw_button(button.x, button.y, button.width, button.height, BUTTON_COLOR,
                         interfaces[buttons.index(button_data)].upper(), selected=not active)
 
-        # Rysowanie przycisku "Start"
         draw_button(start_button[0].x, start_button[0].y, start_button[0].width, start_button[0].height, BUTTON_COLOR,
-                    "Start", selected=not start_button[1])
+                    "Start", selected=not start_button[1], enabled=selected_button is not None)
 
-        # Obsługa zdarzeń
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # Sprawdzamy, czy kliknięto lewym przyciskiem myszy
+                if event.button == 1:
                     for i, button_data in enumerate(buttons):
                         button, active = button_data
                         if button.collidepoint(event.pos) and active:
                             print(f"Kliknięto przycisk dla interfejsu: {interfaces[i]}")
                             selected_button = interfaces[i]
-                            # Po kliknięciu wyłączamy pozostałe przyciski
                             for j in range(len(buttons)):
                                 if j != i:
                                     buttons[j] = (buttons[j][0], False)
                             break
-                    # Obsługa przycisku "Start"
-                    if start_button[0].collidepoint(event.pos) and not start_button[1]:
-                        print(f"Kliknięto przycisk Start and selected:{selected_button}")
-                        # Tutaj możesz dodać logikę dla przycisku Start
+                    if start_button[0].collidepoint(event.pos) and selected_button is not None and not start_button[1]:
+                        print(f"Kliknięto przycisk Start and selected: {selected_button}")
                         start_button = (start_button[0], True)
-                        sniff_packets(selected_button)
+                        stop_event.clear()
+                        sniffer_thread = threading.Thread(target=sniff_packets, args=(selected_button,))
+                        sniffer_thread.start()
 
         pygame.display.flip()
 
--- my_sniffer.py
+++ my_sniffer.py
@@ -1,10 +1,10 @@
 from scapy.all import sniff
+from scapy.all import wrpcap
 
 
 def sniff_packets(interface, count=10):
     print(f"Przechwytywanie pakietów z interfejsu {interface}...")
 
-    # Wywołanie funkcji sniff() z podanym interfejsem i liczbą pakietów do przechwycenia
     packets = sniff(iface=interface, count=count)
 
     for packet in packets:
@@ -15,3 +15,16 @@ def sniff_packets(interface, count=10):
         # Tutaj możesz dodać więcej informacji w zależności od potrzeb
         print("Zawartość:", packet.summary())  # Wyświetla krótkie podsumowanie pakietu
         print()
+
+    save_packets_to_file(packets)
+
+
+def save_packets_to_file(packets, filename="captured_packets.txt"):
+    with open(filename, 'w') as file:
+        for packet in packets:
+            file.write("----------- Pakiet -----------\n")
+            file.write(f"Adres źródłowy: {packet.src}\n")
+            file.write(f"Adres docelowy: {packet.dst}\n")
+            file.write(f"Typ protokołu: {packet.payload.name}\n")
+            file.write("Zawartość: " + packet.summary() + "\n\n")
+    print(f"Pakiety zapisane do pliku {filename}")
